9b1ad6dd5e254575ff122398ab9880f3e007220e
Leandro Castilho <leandro.castilho91@gmail.com>
2018-12-07 14:34:45 -0200

    criando uma pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Categories', component: 'CategoriesPage' },
+      { title: 'Profile', component: 'ProfilePage'},
     ];
 
   }
