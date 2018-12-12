182b41bd3ab48dc438b8a661213d892d6b4b651a
Leandro Castilho <leandro.castilho91@gmail.com>
2018-12-12 16:24:39 -0200

    criando form group para controlar o formulario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['', [Validators.required, Validators.email]],
+      type: ['1', [Validators.required]],
+      individualOrPartyDoc: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      password: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      number: ['', [Validators.required]],
+      addAddress: ['', []],
+      neighborhood: ['', []],
+      zipCode: ['', [Validators.required]],
+      phone1: ['', [Validators.required]],
+      phone2: ['', []],
+      phone3: ['', []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  signupUser(){
+    console.log('Enviou o form');
   }
 
 }
